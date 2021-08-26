575c77b949ec2c9fcb74dbd21b7b25d3547e8fe1
Kringelum2 <p.kringelum@gmail.com>
2021-08-26 11:35:30 +0200

    Task til intro

---
+++ 1._intro/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Systemos.out.println("Spurgt");
+        Systemos.out.println(5);
+    }
+
+}
---
+++ 1._intro/Systemos.java
@@ -0,0 +1,14 @@
+public class Systemos {
+
+    public static class out{
+
+    public static void println(String s) {
+        System.out.println(s);
+    }
+
+    public static void println(int i) {
+        System.out.println(i);
+    }
+
+    }
+}

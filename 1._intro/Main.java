9d441a4cf3992fdfddf61ff9140badeda893d244
Kringelum2 <p.kringelum@gmail.com>
2021-08-26 11:45:50 +0200

    Task til intro

--- 1._intro/Main.java
+++ 1._intro/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Systemos.out.println("Spurgt");
         Systemos.out.println(5);
+        Systemos.out.println(5.69);
     }
 
 }
--- 1._intro/Systemos.java
+++ 1._intro/Systemos.java
@@ -10,5 +10,29 @@ public class Systemos {
         System.out.println(i);
     }
 
+    public static void println(double o) {
+        System.out.println(o);
+    }
+
+    public static void println(float f) {
+        System.out.println(f);
+    }
+
+        public static void println(long l) {
+            System.out.println(l);
+        }
+
+        public static void println(byte b) {
+            System.out.println(b);
+        }
+
+        public static void println(char c) {
+            System.out.println(c);
+        }
+
+        public static void println(boolean b) {
+            System.out.println(b);
+        }
+
     }
 }
